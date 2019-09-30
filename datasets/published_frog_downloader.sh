0434ca867977ca6a898926da12b42b117def48f6
KushnirDmytro <kushnir.dmytro.93@gmail.com>
2019-09-30 13:20:19 +0300

    downloader bugs fixed

--- datasets/published_frog_downloader.sh
+++ datasets/published_frog_downloader.sh
@@ -2,7 +2,7 @@
 
 . ./gdrive_public_file_downloader.sh  --source-only  # importing function to use
 
-DATASET_NAME=published_frog_testdata
+DATASET_NAME=published_test_frog
 ARCHIVE_NAME="/tmp/${DATASET_NAME}.zip"
 # https://drive.google.com/open?id=1MFpJGMrDTFNN274GbrhYsA3FRvO0nUDV
 DATASET_GDRIVE_ID=1MFpJGMrDTFNN274GbrhYsA3FRvO0nUDV
@@ -11,9 +11,9 @@ echo "Downloading dataset [${DATASET_NAME}] as [${ARCHIVE_NAME}]"
 gdrive_download ${DATASET_GDRIVE_ID} ${ARCHIVE_NAME}
 
 echo "Unpacking..."
-unizip -z ${ARCHIVE_NAME} -d `pwd`
+unzip -q ${ARCHIVE_NAME} -d `pwd`
 
-echo "Deleting temps:"
+echo "Deleting temps"
 rm ${ARCHIVE_NAME}
 
 echo "Result: "
