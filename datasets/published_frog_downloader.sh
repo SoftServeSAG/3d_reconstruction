3941c4a0bf8e7812d7b73c34988ac4cd2882ef81
KushnirDmytro <kushnir.dmytro.93@gmail.com>
2019-09-30 13:11:36 +0300

    downloader done

--- datasets/published_frog_downloader.sh
+++ datasets/published_frog_downloader.sh
@@ -3,13 +3,18 @@
 . ./gdrive_public_file_downloader.sh  --source-only  # importing function to use
 
 DATASET_NAME=published_frog_testdata
-ARCHIVE_NAME="${DATASET_NAME}.zip"
+ARCHIVE_NAME="/tmp/${DATASET_NAME}.zip"
 # https://drive.google.com/open?id=1MFpJGMrDTFNN274GbrhYsA3FRvO0nUDV
 DATASET_GDRIVE_ID=1MFpJGMrDTFNN274GbrhYsA3FRvO0nUDV
 
-echo "Downloading dataset [${DATASET_NAME}]"
+echo "Downloading dataset [${DATASET_NAME}] as [${ARCHIVE_NAME}]"
+gdrive_download ${DATASET_GDRIVE_ID} ${ARCHIVE_NAME}
 
 echo "Unpacking..."
+unizip -z ${ARCHIVE_NAME} -d `pwd`
+
+echo "Deleting temps:"
+rm ${ARCHIVE_NAME}
 
 echo "Result: "
 ls -l $DATASET_NAME
