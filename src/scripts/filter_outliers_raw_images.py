f52d950c6d53f664440b576d297a37c0780d85d6
DmytroKushnirSoftserveinc <dkushn@softserveinc.com>
2019-10-02 22:35:54 +0300

    preprocessing as isolated stage refactored and configured

--- src/scripts/filter_outliers_raw_images.py
+++ src/scripts/filter_outliers_raw_images.py
@@ -1,11 +1,8 @@
-# TODO read all raw images from files and perform filering stages:
- # -- using OCV remove small nonzero clusrters
+# -- using OCV remove small nonzero clusrters
 # drop noisy (blurry) images from dataset at all
-# align color of remaining images somehow (# TODO --- how it works?)
-
+# align color of remaining images somehow
 
 import open3d as o3d
-# import opencv as cv
 import cv2
 import copy
 import numpy as np
@@ -18,14 +15,13 @@ from matplotlib import pyplot as plt
 import sys, os
 from time import time
 
-
-sys.path.append("../Utility")
+sys.path.append("../utils")
 from object_reconstruction_config import get_config
 
 from file import get_file_list, make_clean_folder
 
 def display_inlier_outlier(cloud, ind):
-    # TODO
+    # TODO for pointcloud clustering
     inlier_cloud = cloud.select_down_sample(ind)
     outlier_cloud = cloud.select_down_sample(ind, invert=True)
 
@@ -36,20 +32,21 @@ def display_inlier_outlier(cloud, ind):
 
 # returns list of masks, all exept of largest contour blob is ignored
 def make_masks_from_countures(depth_images_filenames):
-    depth_images = [cv2.imread(fname, cv2.CV_8UC1) for fname in depth_images_filenames]
-    depth_preprocessed = [ cv2.GaussianBlur(im, (5, 5), 5) for im in depth_images ]
+    depth_images = [cv2.imread(fname, cv2.CV_8UC1)
+                    for fname in depth_images_filenames]
+    depth_preprocessed = [ cv2.GaussianBlur(im, (5, 5), 5)
+                           for im in depth_images ]
 
-    # TODO use advanced thresholding https://docs.opencv.org/master/d7/d4d/tutorial_py_thresholding.html#gsc.tab=0
-    binary_masks = [ (cv2.threshold(d, 0, 255, cv2.THRESH_OTSU))[1] for d in depth_preprocessed]
+    # other approaches for thresholding https://docs.opencv.org/master/d7/d4d/tutorial_py_thresholding.html#gsc.tab=0
+    binary_masks = [(cv2.threshold(d, 0, 255, cv2.THRESH_OTSU))[1]
+                     for d in depth_preprocessed]
 
-    countours = [
-        (cv2.findContours(bm, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE))[0] for bm in binary_masks
-    ]
-    largest_contour = [
-        max(contours_list, key=lambda x: cv2.contourArea(x))
-        for contours_list in countours
-    ]
-    masks = [ np.zeros_like(dp_im) for dp_im in depth_images]
+    countours = [(cv2.findContours(bm, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE))[0]
+                  for bm in binary_masks]
+    largest_contour = [max(contours_list, key=lambda x: cv2.contourArea(x))
+                       for contours_list in countours]
+    masks = [ np.zeros_like(dp_im)
+              for dp_im in depth_images]
     for i, mask in enumerate(masks):
         cv2.drawContours(mask, [largest_contour[i]], -1, (255), cv2.FILLED)
     return masks
@@ -133,37 +130,39 @@ def color_based_clustering_masks(image_filename, color_clustering_config, debug_
     contours, _ = cv2.findContours(masking, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     cont_len_thresh = color_clustering_config['blob_contour_min_length']
-    filtered_controus = [ contour for contour in contours if cv2.contourArea(contour) > cont_len_thresh ]
-
-
+    filtered_controus = [ contour for contour in contours
+                          if cv2.contourArea(contour) > cont_len_thresh ]
 
     masking_filtered = np.zeros_like(masking)
     cv2.drawContours(masking_filtered, filtered_controus, -1, (255), cv2.FILLED)
-
 
     if debug_mode:
         masking = np.zeros_like(masking)
         cv2.drawContours(masking, contours, -1, (255), cv2.FILLED)
 
-        print(biggest_cluster_index)
-        print(biggest_cluster_n_elements)
+        print("Largest cluster Index : [{}], has_points: [{}] "
+              .format(biggest_cluster_index, biggest_cluster_n_elements))
 
         # assume that max size cluster is the background
-
         quant = clt.cluster_centers_.astype("uint8")[labels]
         # quant_masks = clt.cluster_centers_.astype("uint8")[mask]
 
-        print(masking.shape)
-        print(labels.shape)
-        print(quant.shape)
+        print("labels shape: [{}]".format(labels.shape))
+
+        print("Shapes after clusteting :")
+        print("mask : [{}]".format(masking.shape))
+        print("quantized image : [{}]".format(masking.shape))
+        print()
 
         # reshape the feature vectors to images
         quant = quant.reshape((h, w, 3))
         image = image.reshape((h, w, 3))
 
-        print(masking.shape)
-        print(labels.shape)
-        print(quant.shape)
+        print("Reshaped back to pictures :")
+        print("mask : [{}]".format(masking.shape))
+        print("quantized image : [{}]".format(masking.shape))
+        print()
+
 
         # convert from L*a*b* to RGB
         quant = cv2.cvtColor(quant, cv2.COLOR_LAB2BGR)
@@ -172,17 +171,31 @@ def color_based_clustering_masks(image_filename, color_clustering_config, debug_
         # display the images and wait for a keypress
         cv2.imshow("image", np.hstack([image,
                                        quant,
-                                       cv2.cvtColor(masking, cv2.COLOR_GRAY2BGR),
-                                       cv2.cvtColor(masking_filtered, cv2.COLOR_GRAY2BGR)
+                                       cv2.cvtColor(masking, cv2.COLOR_GRAY2BGR)
                                        ]))
         cv2.waitKey(0)
     return masking_filtered
 
+DEFAULT_CONFIFG_FILENAME = '../../cfg/reconstruction/default/preprocessing_raw_images.json'
 
-DEFAULT_CONFIFG_FILENAME = 'cfg/reconstruction/default/filter_outlier_raw_images.json'
+def list_dataset_files(config):
+    input_color_path = os.path.join(config['path_dataset'], config['color_dir'])
+    input_depth_path = os.path.join(config['path_dataset'], config['depth_dir'])
 
-def get_dataset_files(config):
-    return color_images, depth_images
+    assert os.path.exists(config['path_dataset']) and \
+           os.path.exists(input_color_path) and \
+           os.path.exists(input_depth_path), \
+        "Attempt to read dataset failed"
+
+    depth_im_filenames = get_file_list(input_depth_path,
+                                     extension=".png")
+    color_im_filenames = get_file_list(input_color_path,
+                                     extension=".jpg")
+
+    assert (len(depth_im_filenames) == len(color_im_filenames)), \
+        "Expected equal number of depth and rgb images"
+
+    return color_im_filenames, depth_im_filenames
 
 if __name__ == "__main__":
 
@@ -191,33 +204,20 @@ if __name__ == "__main__":
     if debug_mode:
         o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Debug)
 
-    input_color_path = os.path.join(config['path_dataset'], config['color_dir'])
-    input_depth_path = os.path.join(config['path_dataset'], config['depth_dir'])
-
-    assert os.path.exists(config['path_dataset']) and \
-           os.path.exists(input_color_path) and \
-           os.path.exists(input_depth_path) , \
-                "Attempt to read dataset failed"
-
     output_color_path = os.path.join(config['path_dataset'], config['color_result_dir'])
     output_depth_path = os.path.join(config['path_dataset'], config['depth_result_dir'])
 
     make_clean_folder(output_color_path)
     make_clean_folder(output_depth_path)
 
-    depth_images = []
-    color_images = []
-
     camera_intrinsics = o3d.io.read_pinhole_camera_intrinsic(config['path_intrinsic'])
 
-    depth_image_path = get_file_list(input_depth_path,
-                                     extension=".png")
-    color_image_path = get_file_list(input_color_path,
-                                     extension=".jpg")
-    color_image_path = color_image_path[:]
-    depth_image_path = depth_image_path[:]
+    input_rgb_images_files, input_depth_images_files = list_dataset_files(config)
 
-    assert (len(depth_image_path) == len(color_image_path))
+    if not config['process_all_images']:
+        input_rgb_images_files = input_rgb_images_files[config['dataset_range'][0]:config['dataset_range'][1]]
+        input_depth_images_files = input_depth_images_files[config['dataset_range'][0]:config['dataset_range'][1]]
+
     first = True
 
     # blob_detectot = cv2.SimpleBlobDetector()
@@ -228,7 +228,7 @@ if __name__ == "__main__":
     start_time = time()
 
     masks = []
-    if config['use_color_cluster_thresholds']:
+    if config['color_clusters_thresholding']:
 
         color_clustering_config = config['threshold_colored_clusters']
 
@@ -236,30 +236,31 @@ if __name__ == "__main__":
             from joblib import Parallel, delayed
             import multiprocessing
             MAX_THREAD = min(multiprocessing.cpu_count(),
-                             max(len(color_image_path), 1))
+                             max(len(input_rgb_images_files), 1))
             print("[Running in [{}] threads mode]".format(MAX_THREAD))
 
             results = Parallel(n_jobs=MAX_THREAD)(delayed(
                 color_based_clustering_masks)(col_im_path, color_clustering_config)
-                                                  for col_im_path in tqdm(color_image_path) )
+                                                  for col_im_path in tqdm(input_rgb_images_files))
             masks.extend(results)
         else:
             print("[Running in single thread mode]")
-            for col_im_path in tqdm(color_image_path):
+            for col_im_path in tqdm(input_rgb_images_files):
                 masks.append(color_based_clustering_masks(
                     col_im_path,
-                    color_clustering_config
+                    color_clustering_config,
+                    debug_mode=debug_mode
                 )
                 )
 
-    elif config['blobs_len_thresholding']:  # this clustering approach uses length of cluster border as the basis for filtering
-        print("blobs_len_thresholding")
-        masks = make_masks_from_countures_for_loop(depth_image_path)
+    elif config['blobs_border_len_thresholding']:
+        print("blobs_border_len_thresholding")
+        masks = make_masks_from_countures(input_depth_images_files)
 
 
     print("End computing masks {}".format(time() - start_time))
-    depth_images = [cv2.imread(fname, cv2.IMREAD_ANYDEPTH) for fname in depth_image_path]
-    color_images = [cv2.imread(fname,cv2.COLOR_BGR2RGB) for fname in color_image_path]
+    depth_images = [cv2.imread(fname, cv2.IMREAD_ANYDEPTH) for fname in input_depth_images_files]
+    color_images = [cv2.imread(fname,cv2.COLOR_BGR2RGB) for fname in input_rgb_images_files]
 
     print("Applying mask to depth")
     masked_depth_images = [
@@ -273,7 +274,7 @@ if __name__ == "__main__":
         for im, mask in tqdm(zip(color_images, masks))
     ]
 
-    if debug_mode:
+    if config['show_all_results']:
         for mask in masked_color_images:
             plt.imshow(mask)
             plt.show()
